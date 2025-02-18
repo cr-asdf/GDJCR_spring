b3c7d8b52817e0b3cf3aa1b5dfe4f0f9f7c192b8
merandie <kimdaeki@gmail.com>
2025-02-18 14:29:33 +0900

    1

---
+++ src/main/java/com/winter/app/parts/Arm.java
@@ -0,0 +1,7 @@
+package com.winter.app.parts;
+
+public interface Arm {
+	
+	public abstract void punch();
+
+}
---
+++ src/main/java/com/winter/app/parts/LeftArm.java
@@ -0,0 +1,14 @@
+package com.winter.app.parts;
+
+import org.springframework.stereotype.Component;
+
+
+public class LeftArm implements Arm{
+	
+	@Override
+	public void punch() {
+		System.out.println("로켓 발사");
+		
+	}
+
+}
